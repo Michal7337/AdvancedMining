ea7a3ea94dda028d7e7df66031cad4cfcec237ef
mgrze <mgrzesik737@gmail.com>
2025-06-17 16:26:19 +0200

    Update plugin to 1.21.6

--- src/main/java/win/codingboulder/advancedmining/CustomBlockArgument.java
+++ src/main/java/win/codingboulder/advancedmining/CustomBlockArgument.java
@@ -8,7 +8,6 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import org.jetbrains.annotations.NotNull;
 
